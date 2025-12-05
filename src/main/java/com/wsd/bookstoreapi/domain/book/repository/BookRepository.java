115e9b7ddf5780f607ec21f99bc5ba78dabb82fe
JC-arl <hwang1535@naver.com>
2025-12-05 23:03:41 +0900

    BookRepository 검색 + 페이지네이션 + 정렬

---
+++ src/main/java/com/wsd/bookstoreapi/domain/book/repository/BookRepository.java
@@ -0,0 +1,32 @@
+package com.wsd.bookstoreapi.domain.book.repository;
+
+import com.wsd.bookstoreapi.domain.book.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    /**
+     * keyword: title, author, description에 LIKE 검색
+     * category: 정확 일치
+     *
+     * 둘 중 하나 혹은 둘 다 null이면 해당 조건은 무시
+     */
+    @Query("""
+           SELECT b
+           FROM Book b
+           WHERE (:keyword IS NULL
+                  OR LOWER(b.title) LIKE LOWER(CONCAT('%', :keyword, '%'))
+                  OR LOWER(b.author) LIKE LOWER(CONCAT('%', :keyword, '%'))
+                  OR LOWER(b.description) LIKE LOWER(CONCAT('%', :keyword, '%')))
+             AND (:category IS NULL OR b.category = :category)
+           """)
+    Page<Book> searchBooks(
+            @Param("keyword") String keyword,
+            @Param("category") String category,
+            Pageable pageable
+    );
+}
